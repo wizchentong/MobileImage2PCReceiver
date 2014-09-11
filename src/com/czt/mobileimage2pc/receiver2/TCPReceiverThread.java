f4d1586472ee31ad16defd89e11986f15d35714f
wizchentong <chentong@wiz.cn>
2014-09-11 10:51:35 +0800

    优化

--- src/com/czt/mobileimage2pc/receiver2/TCPReceiverThread.java
+++ src/com/czt/mobileimage2pc/receiver2/TCPReceiverThread.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TCPReceiverThread extends Thread {
-	private boolean mIsTCPRunning = false;
 	@Override
 	public void run() {
 		ParseThread2 parseThread = new ParseThread2();
@@ -25,14 +24,12 @@ public class TCPReceiverThread extends Thread {
 				continue;
 			}
 			
-			mIsTCPRunning = true;
 			try {
-				// 客户端请求与本机在19586端口建立TCP连接
-				Socket client = new Socket(mAddress.remove(0), 19586);
-				client.setSoTimeout(10000);
+				mTCPClient = new Socket(mAddress.remove(0), 19586);
+				mTCPClient.setSoTimeout(10000);
 				// 获取Socket的输入流，用来接收从服务端发送过来的数据
 				BufferedReader buf = new BufferedReader(new InputStreamReader(
-						client.getInputStream()));
+						mTCPClient.getInputStream()));
 				while (true) {
 					try {
 						String str = buf.readLine();
@@ -44,24 +41,30 @@ public class TCPReceiverThread extends Thread {
 						System.out.println("Time out, No response");
 					}
 				}
-				if (client != null) {
+				if (mTCPClient != null) {
 					// 如果构造函数建立起了连接，则关闭套接字，如果没有建立起连接，自然不用关闭
-					client.close(); // 只关闭socket，其关联的输入输出流也会被关闭
+					mTCPClient.close(); // 只关闭socket，其关联的输入输出流也会被关闭
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			mIsTCPRunning = false;
 			
 		}
 	}
 
 	private List<InetAddress> mAddress = new ArrayList<InetAddress>();
+	private Socket mTCPClient;
 
 	public void add(InetAddress ip, String userGuid) {
 		// 当前账户有效,非当前用户不接受
-		if (userGuid.contains("6f5de636-1d09-4876-9457-d03063b39adf") && !mIsTCPRunning) {
+		if (userGuid.contains("6f5de636-1d09-4876-9457-d03063b39adf") && !isTCPRunning()) {
 			mAddress.add(ip);
 		}
+	}
+	private boolean isTCPRunning(){
+		if(mTCPClient != null && mTCPClient.isConnected()){
+			return true;
+		}
+		return false;
 	}
 }
